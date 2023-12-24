2a958b703d7ce2121ed45ae253d166d054450cf9
Ross Burchnall <r-burchnall@hotmail.co.uk>
2023-12-24 13:11:20 +0000

    added futures

--- interest-giver/src/main.rs
+++ interest-giver/src/main.rs
@@ -23,88 +23,61 @@ async fn main() {
     let rpc_url = "http://localhost:8899".to_string();
     let connection = RpcClient::new_with_commitment(rpc_url.to_string(), CommitmentConfig::confirmed());
 
-    let accounts = connection.get_program_accounts(&PROGRAM_ID);
-    println!("Accounts: {:?}", accounts);
+    let accounts = connection.get_program_accounts(&PROGRAM_ID).unwrap();
 
     let non_blocking_client = solana_client::nonblocking::rpc_client::RpcClient::new_with_commitment(
         rpc_url.to_string(),
         CommitmentConfig::confirmed(),
     );
 
-    // let mut requests = vec![];
-    let accounts = accounts.unwrap();
-    for account in accounts {
-        let vault_pubkey = account.0;
-        println!("Applying interest to account: {}", vault_pubkey);
+    let non_blocking_client = Arc::new(non_blocking_client);
 
-        let account = account.1;
-        let mut buf: &[u8] = account.data.as_slice();
-        let account = InterestVault::try_deserialize(&mut buf).unwrap();
+    let recent_blockhash = connection.get_latest_blockhash().unwrap();
 
-        let to_ata = account.ata_address;
-        let from_ata = account.ata_address;
+    let requests = accounts
+        .iter()
+        .map(|acc| {
+            let account = acc.clone();
+            let vault_pubkey = account.0;
 
-        let ix = Instruction::new_with_bytes(
-            PROGRAM_ID,
-            &[0],
-            vec![
-                AccountMeta::new(interest_payer_keypair.pubkey(), true), // signer
-                AccountMeta::new(vault_pubkey, false), // vault
-                AccountMeta::new(to_ata, false), //toata
-                AccountMeta::new(from_ata, false), //fromata
-                AccountMeta::new(TOKEN_PROGRAM_ID, false) //token_program
-            ],
-        );
+            let account = account.1;
+            let mut buf: &[u8] = account.data.as_slice();
+            let account = InterestVault::try_deserialize(&mut buf).unwrap();
 
-        let recent_blockhash = connection.get_recent_blockhash().unwrap().0;
+            let to_ata = account.ata_address;
+            let from_ata = interest_payer_keypair.pubkey();
 
-        let tx: Transaction = Transaction::new_signed_with_payer(
-            &[ix],
-            Some(&interest_payer_keypair.pubkey()),
-            &[&interest_payer_keypair],
-            recent_blockhash,
-        );
+            let ix = Instruction::new_with_bytes(
+                PROGRAM_ID,
+                &[0],
+                vec![
+                    AccountMeta::new(interest_payer_keypair.pubkey(), true), // signer
+                    AccountMeta::new(vault_pubkey, false), // vault
+                    AccountMeta::new(to_ata, false), //toata
+                    AccountMeta::new(from_ata, false), //fromata
+                    AccountMeta::new(TOKEN_PROGRAM_ID, false) //token_program
+                ],
+            );
 
-        let tx = non_blocking_client.send_and_confirm_transaction(&tx).await;
-        println!("Tx: {:?}", tx);
+            let tx: Transaction = Transaction::new_signed_with_payer(
+                &[ix],
+                Some(&interest_payer_keypair.pubkey()),
+                &[&interest_payer_keypair],
+                recent_blockhash,
+            );
 
-        // // // let client: Arc<solana_client::nonblocking::rpc_client::RpcClient> = Arc::clone(&non_blocking_client);
-        // // let request = tokio::spawn(async move {
-        // //     let client = client;
-        // //     client.send_and_confirm_transaction(&tx).await;
-        // // });
-        //
-        // requests.push(request);
-    }
+            let client = Arc::clone(&non_blocking_client);
+            tokio::task::spawn(async move {
+                println!("Applying interest to account: {}", vault_pubkey);
+                match client.send_and_confirm_transaction(&tx).await {
+                    Ok(_) => println!("Success"),
+                    Err(e) => println!("Error for {}: {:?}", vault_pubkey, e),
+                }
+            })
+        })
+        .collect::<Vec<_>>();
+
+    futures::future::join_all(requests).await;
 
     println!("Completed");
-    // join_all(requests).await;
 }
-//
-// fn send_initialize_tx(
-//     client: &RpcClient,
-//     program_id: Pubkey,
-//     payer: &Keypair
-// ) -> Result<()> {
-//
-//     let ix = gfx_token_vaults::transaction::Transaction;
-//
-//     let instruction = Instruction::new_with_borsh(
-//         program_id,
-//         &ix,
-//         vec![
-//
-//         ],
-//     );
-//
-//     let blockhash = client.get_latest_blockhash()?;
-//     let mut tx = Transaction::new_signed_with_payer(
-//         &[instruction],
-//         Some(&payer.pubkey()),
-//         &[payer],
-//         blockhash,
-//     );
-//     client.send_and_confirm_transaction(&tx)?;
-//
-//     Ok(())
-// }
